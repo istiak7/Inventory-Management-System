81e5de12842a8745c0b846229b2f2bf7fb003478
istiak7 <istiak6069@gmail.com>
2026-07-17 17:55:56 +0600

    added pagination extension method

---
+++ Shared/Extensions/PaginationExtensions/EFCorePaginationExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace Inventory_Management_System.Shared.Extensions.PaginationExtensions
+{
+    public static class EFCorePaginationExtensions
+    {
+        public static async Task<PagedResult<T>> ToPagedResultAsync<T>(
+            this IQueryable<T> source,
+            int pageNumber,
+            int pageSize,
+            CancellationToken cancellationToken = default)
+        {
+
+            if (pageNumber < 1) pageNumber = 1;
+            if (pageSize < 1) pageSize = 20;
+
+
+            var totalCount = await source.LongCountAsync(cancellationToken);
+            var offset = (pageNumber - 1) * pageSize;
+
+            var items = await source
+                .Skip(offset)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new PagedResult<T>
+            {
+                Items = items,
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalCount = totalCount
+            };
+        }
+    }
+}
---
+++ Shared/Extensions/PaginationExtensions/PageResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Inventory_Management_System.Shared.Extensions.PaginationExtensions
+{
+    public sealed class PagedResult<T>
+    {
+        public IReadOnlyList<T> Items { get; init; } = [];
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public long TotalCount { get; init; }
+        public int TotalPages => PageSize <= 0 ? 0 : (int)Math.Ceiling(TotalCount / (double)PageSize);
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
+    }
+}
