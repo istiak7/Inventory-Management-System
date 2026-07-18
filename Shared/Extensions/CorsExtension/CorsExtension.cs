24c01dad714608a4173b254fe0b10c5ec05f3078
istiak7 <istiak6069@gmail.com>
2026-07-18 23:48:59 +0600

    config Cors

---
+++ Shared/Extensions/CorsExtension/CorsExtension.cs
@@ -0,0 +1,21 @@
+﻿namespace Inventory_Management_System.Shared.Extensions.CorsExtension
+{
+    public static class CorsExtension
+    {
+        public static IServiceCollection AddCorsExtension(this IServiceCollection services, IConfiguration configuration)
+        {
+            var allowedOrigins = configuration.GetSection("AllowedOrigins").Get<string[]>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                {
+                    builder.WithOrigins(allowedOrigins)
+                           .AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowCredentials();
+                });
+            });
+            return services;
+        }
+    }
+}
