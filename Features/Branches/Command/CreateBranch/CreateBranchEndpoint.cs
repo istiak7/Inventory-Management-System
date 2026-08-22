6c23a88c30fab434a840796af5a8455bfdf81213
istiak7 <istiak6069@gmail.com>
2026-08-22 15:13:50 +0600

    Add permission check to create branch endpoint
    Added .RequirePermission(Permissions.BranchesManage) to the /create-branches POST endpoint to restrict access to users with the appropriate permission. This enhances security by ensuring only authorized users can create branches.

--- Features/Branches/Command/CreateBranch/CreateBranchEndpoint.cs
+++ Features/Branches/Command/CreateBranch/CreateBranchEndpoint.cs
@@ -11,7 +11,7 @@ namespace Inventory_Management_System.Features.Branches.Command.CreateBranch
             {
                 var result = await mediator.Send(command);
                 return Results.Ok(result);
-            }).WithTags("Branch");
+            }).WithTags("Branch").RequirePermission(Permissions.BranchesManage);
         }
     }
 }
