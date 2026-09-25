1fde81197e7f08e5d93039e116ce9f78b1115dbf
istiak7 <istiak6069@gmail.com>
2026-09-25 20:01:52 +0600

    added migration file

---
+++ Migrations/20260925135344_added new property office expense.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Inventory_Management_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class addednewpropertyofficeexpense : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "Expenses",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedById",
+                table: "Expenses",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "ExpenseCategories",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UpdatedById",
+                table: "ExpenseCategories",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedById",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "ExpenseCategories");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedById",
+                table: "ExpenseCategories");
+        }
+    }
+}
